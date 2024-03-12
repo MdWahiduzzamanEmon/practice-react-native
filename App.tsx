51f73ad63401b5856149ec962213d9a1aabbb192
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2024-03-13 01:38:06 +0600

    Add TextInput component to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 const App = () => {
+  const [text, setText] = React.useState('');
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
         {'Welcome to React Native with TypeScript!'}
       </Text>
+
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+          width: 200,
+          margin: 10,
+        }}
+        onChangeText={text => setText(text)}
+        value={text}
+      />
     </View>
   );
 };
